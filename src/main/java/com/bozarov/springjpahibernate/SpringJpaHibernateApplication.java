d71d90e888757f9f1477d66d2e89b49d5dbdb62e
John Bozarov <codinginusa@gmail.com>
2020-09-30 12:56:39 -0400

    second person jacob created

--- src/main/java/com/bozarov/springjpahibernate/SpringJpaHibernateApplication.java
+++ src/main/java/com/bozarov/springjpahibernate/SpringJpaHibernateApplication.java
@@ -45,12 +45,6 @@ public class SpringJpaHibernateApplication implements CommandLineRunner{
 		john.setName("Jahongir");
 		Person savedJohn = personRepository.save(john); 
 		
-		// Person Jacob 
-		Person jacob = new Person(); 
-		jacob.setName("Omonjon");
-		Person savedJacob = personRepository.save(jacob); 
-		
-		
 		Account johnAccount = new Account(21551556543L, savedJohn); 
 		Account savedJohnAccount = accountRepository.save(johnAccount);
 		savedJohn.setAccount(savedJohnAccount);
@@ -61,111 +55,38 @@ public class SpringJpaHibernateApplication implements CommandLineRunner{
 		Car savedCorolla = carRepository.save(corolla); 
 		Car rx350 = new Car("Toyota RX350", savedJohn); 
 		Car savedRx350 = carRepository.save(rx350); 
-		Set<Car> savedCars = new HashSet<>(); 
-		savedCars.add(savedSienna);
-		savedCars.add(savedCorolla);
-		savedCars.add(savedRx350); 
-		savedJohn.setCars(savedCars);
+		Set<Car> savedJohnCars = new HashSet<>(); 
+		savedJohnCars.add(savedSienna);
+		savedJohnCars.add(savedCorolla);
+		savedJohnCars.add(savedRx350); 
+		savedJohn.setCars(savedJohnCars);
 		
-//		Hobby soccer = new Hobby("Soccer", "Playing soccer game like a Messi!", savedJohn); 
-//		personRepository.save(savedJohn); 
+		// Person Jacob 
+		Person jacob = new Person(); 
+		jacob.setName("Omonjon");
+		Person savedJacob = personRepository.save(jacob); 
+		
+		Account jacobAccount = new Account(876435843483L, savedJacob); 
+		Account savedJacobAccount = accountRepository.save(jacobAccount);
+		savedJacob.setAccount(savedJacobAccount);
+		
+		Car altima = new Car("Nissan Altima", savedJacob); 
+		Car savedAltima = carRepository.save(altima); 
+		Car bmw = new Car("BMW X6", savedJacob);
+		Car savedBmw = carRepository.save(bmw); 
+		Set<Car> savedJacobCars = new HashSet<>(); 
+		savedJacobCars.add(savedAltima); 
+		savedJacobCars.add(savedBmw); 
+		savedJacob.setCars(savedJacobCars);
+		
+		
+		
+		//Hobby soccer = new Hobby("Soccer", "Playing soccer game like a Messi!", savedJohn); 
+		personRepository.save(savedJohn); 
+		personRepository.save(savedJacob); 
 		
 	}
 	
 }
 
-
-
-
-//package com.bozarov.springjpahibernate;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//import com.bozarov.springjpahibernate.domain.Account;
-//import com.bozarov.springjpahibernate.domain.Car;
-//import com.bozarov.springjpahibernate.domain.Person;
-//import com.bozarov.springjpahibernate.repos.AccountRepository;
-//import com.bozarov.springjpahibernate.repos.CarRepository;
-//import com.bozarov.springjpahibernate.repos.PersonRepository;
-//
-//@SpringBootApplication
-//public class SpringJpaHibernateApplication implements CommandLineRunner{
-//
-//	private final PersonRepository personRepository;
-//	private final AccountRepository accountRepository;
-//	private final CarRepository carRepository;
-//
-//
-//	public SpringJpaHibernateApplication(PersonRepository personRepository, AccountRepository accountRepository,
-//			CarRepository carRepository) {
-//		super();
-//		this.personRepository = personRepository;
-//		this.accountRepository = accountRepository;
-//		this.carRepository = carRepository;
-//	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(SpringJpaHibernateApplication.class, args);
-//	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		
-//		Person person = new Person();
-//		person.setName("John Wick");
-//		
-//		//     has an id
-//		Person savedPerson = personRepository.save(person);
-//		
-//		
-//		Account account = new Account();
-//		account.setAccnumber(1234L);
-//		account.setPerson(savedPerson);
-//		
-//		Account savedAccount = accountRepository.save(account);
-//		
-//		savedPerson.setAccount(savedAccount);
-//		
-//		personRepository.save(savedPerson);
-//		
-//		
-//		Car car1 = new Car();
-//		car1.setName("BMW");
-//		car1.setPerson(savedPerson);
-//		Car savedCar1 = carRepository.save(car1);
-//		
-//		Car car2 = new Car();
-//		car2.setName("BMW 1");
-//		car2.setPerson(savedPerson);
-//		Car savedCar2 = carRepository.save(car2);
-//		
-//		Car car3 = new Car();
-//		car3.setName("BMW 2");
-//		Car savedCar3 = carRepository.save(car3);
-//		
-//		Car car4= new Car();
-//		car4.setName("BMW 3");
-//		car4.setPerson(savedPerson);
-//		Car savedCar4 = carRepository.save(car4);
-//		
-//		Set<Car> cars = new HashSet<>();
-//		cars.add(savedCar1);
-//		cars.add(savedCar2);
-//		cars.add(savedCar3);
-//		cars.add(savedCar4);
-//		
-//		savedPerson.setCars(cars);
-//		
-//		personRepository.save(savedPerson);
-//		
-//	}
-//	
-//	
-//
-//}
 
